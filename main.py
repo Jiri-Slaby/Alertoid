1ab9e11088a32ea440381605172919abcf103dee
Jiri-Slaby <slabyjiri1@gmail.com>
2023-08-13 19:39:38 +0200

    Formated code

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-# Add references to discord.py and requests
 import discord
 from discord.ext import commands
 import requests
@@ -6,61 +5,58 @@ import datetime
 import asyncio
 import json
 
-
-intents = discord.Intents.all() # This will enable the default intents
-intents.members = True # This will enable the members intent
+intents = discord.Intents.all()  # This will enable the all intents
+intents.members = True  # This will enable the members intent
 
 # Create a bot instance with a command prefix
 bot = commands.Bot(command_prefix="!", intents=intents)
+# Global Variables
 previous_alert_data = None
 current_alert_data = None
 
 
-# Define a command to get the alert data from the planetside 2 api
 async def alert():
-    # Send a GET request to the api endpoint
     global previous_alert_data
     global current_alert_data
+    # Send a GET request to the api endpoint
     response = requests.get("https://api.ps2alerts.com/instances/active")
 
-    # Check if the response is successful
+    # Check the response
     if response.status_code == 200:
-        # Parse the JSON data into a dictionary
+        # Parse the JSON
         data = response.json()
-        # Initialize a variable to store the alert data
 
-        # Loop over the data
         for item in data:
             # Check if the item has world 10
-            current_alert_data = None;
-            if str(item["world"]) == "10":  # Use str() to convert to string
-                # Store the item in the alert_data variable
+            current_alert_data = None
+            if str(item["world"]) == "10":
+                # Store the item in the alert_data
                 current_alert_data = item
-                # Break out of the loop
                 break
-
-        # Create a dictionary to map the values to the continent names
+        # print of the API return call
         print(current_alert_data)
 
         # Check if the alert_data is not None
         if current_alert_data is not None and current_alert_data["zone"] != previous_alert_data:
+            # Dictionary of continents
             continent_names = {2: "Indar", 4: "Hossin", 6: "Amerish", 8: "Esamir", 344: "Oshur"}
 
-            # Get the value of the censusMetagameEventType key from the current_alert_data variable
+            # Get the value of the zone
             event_type = current_alert_data["zone"]
 
-            # Get the continent name from the dictionary using the value as a key
+            # Get the continent name from the dictionary
             continent_name = continent_names[event_type]
-            # Get the value of the timeStarted key from the alert_data variable
+            # Get the value
             time_started = current_alert_data["timeStarted"]
 
-            # Convert the string to a datetime object using UTC format
+            # Convert the string
             time_started = datetime.datetime.strptime(time_started, "%Y-%m-%dT%H:%M:%S.%fZ")
-            # Create a time interval of 3 hours
+
+            # Create a time interval of 3 hours and how long is the alert running which is 1:30
             three_hours = datetime.timedelta(hours=2)
             alert_time = datetime.timedelta(hours=1, minutes=30)
 
-            # Add 3 hours to the datetime object
+            # Time managment
             time_started_plus_three = time_started + three_hours
             alert_end_time = time_started_plus_three + alert_time
             time_started_plus_three = time_started_plus_three.strftime("%H:%M:%S %d-%m-%Y")
@@ -68,6 +64,7 @@ async def alert():
 
             channel = bot.get_channel(1137502072086999181)
             role_id = 1139520199217905766
+
             # get the role object from the role id
             role = discord.utils.get(channel.guild.roles, id=role_id)
 
@@ -75,21 +72,19 @@ async def alert():
                       f"Start time: {time_started_plus_three}\n" \
                       f"End time: {alert_end_time}\n"
 
-            # Get the channel object with the id 1137502072086999181 using bot.get_channel()
             previous_alert_data = current_alert_data["zone"]
-            print(previous_alert_data)
-            # Send the message to that channel using channel.send()
+
+            # Send the message
             await channel.send(message)
 
         if current_alert_data is None and previous_alert_data is not None:
-            # Send a message that there is no alert on miller server
-
-            # Get the channel object with the id 1137502072086999181 using bot.get_channel()
+            # Get the channel
             channel = bot.get_channel(1137502072086999181)
-
-            # Send the message to that channel using channel.send()
+            # Send the message
             await channel.send("___________________________The alert has ended___________________________")
             previous_alert_data = None
+
+
 @bot.command()
 async def map(ctx):
     base_url = "https://census.daybreakgames.com/s:alertoid/get/ps2:v2/map/"
@@ -105,16 +100,17 @@ async def map(ctx):
 
     # Define the zone ids
     zone_ids = [2, 4, 6, 8, 344]
-    channel = bot.get_channel(1137502072086999181)
+    # Get the channel where the command is called
+    channel = bot.get_channel(ctx)
 
     # Initialize a list to store locked zone information
     locked_zones_info = []
 
     # Loop through the zone ids
     for zone_id in zone_ids:
-        # Construct the full url with query parameters
+        # Construct the full url
         full_url = base_url + "?world_id=" + str(world_id) + "&zone_ids=" + str(zone_id)
-        # Make a GET request and get the response as JSON
+        # Make a GET request
         response = requests.get(full_url).json()
         # Get the map data from the response
         map_data = response["map_list"][0]["Regions"]["Row"]
@@ -124,7 +120,8 @@ async def map(ctx):
         for row in map_data:
             # Get the faction id from the row
             faction_id = row["RowData"]["FactionId"]
-            # Add the faction id to the set if it is not 0 (neutral)
+            # Add the faction id to the set if it is not 0 which are NS, that cannot lock the continent.
+            # API calls return that on Oshur NS are controlling the region even if the continent is locked
             if faction_id != "0":
                 faction_ids.add(faction_id)
 
@@ -139,22 +136,24 @@ async def map(ctx):
             # Continent is not locked
             locked_zones_info.append(f"{zone_id} is OPEN")
 
-    # Join the locked zone information into a single string
+    # Join information into a single string
     locked_zones_summary = "\n".join(locked_zones_info)
 
-    # Send the summary message to the channel
+    # Send message to the channel
     await channel.send(locked_zones_summary)
 
 
 async def loop_alert():
     while True:
-        await alert() # Call the alert function with ctx as an argument
-        await asyncio.sleep(120) # Wait for 60 seconds before repeating
+        await alert()  # Call the alert
+        await asyncio.sleep(120)  # Wait for 120 seconds
+
 
 @bot.event
 async def on_ready():
-    print(f"{bot.user} has connected to Discord!") # Print a message to indicate that the bot is ready
-    await loop_alert() # Call the loop_alert function with None as an argument
+    print(f"{bot.user} has connected to Discord!")  # Connect message
+    await loop_alert()  # Call the loop_alert
 
-# Run the bot with your bot token updated token MTEzNzQ3OTA3MDg4ODc3MTYyNQ.GGMnum._kMg7JLlHL0q2V0_dUq7XGCT34JYBqdB8xl6GI
-bot.run("MTEzNzQ3OTA3MDg4ODc3MTYyNQ.GGMnum._kMg7JLlHL0q2V0_dUq7XGCT34JYBqdB8xl6GI")
+
+# Discord bot token
+bot.run(Bot_token)
